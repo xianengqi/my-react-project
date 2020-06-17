6fdab88e721dbcdc2112a8ae80fad1ea45d8730a
xianengqi <xianengqi@gmail.com>
2020-06-17 20:15:52 +0800

    update package.json or add storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FC} from 'react'
+import { Button } from 'antd'
+import './App.css'
 
-function App() {
+const items = [
+  {
+    "id": 1,
+    "title": "去新疆游玩",
+    "price": 200,
+    "date": "2020-06-18",
+    "category": {
+      "id": 1,
+      "name": "旅行",
+      "type": "outcome"
+    }
+  }
+]
+
+const App: FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="primary">Button</Button>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/ComponentName.tsx
@@ -0,0 +1,9 @@
+import React, {FC} from 'react'
+
+const ComponentName: FC = () => {
+  return (
+    <></>
+  )
+}
+
+export default ComponentName;
