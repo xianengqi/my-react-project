5a0f2cb15852f404b6aae45dd622d178ffa6b7f5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-25 16:10:39 +0000

    Deploy xianengqi/my-react-project to github.com/xianengqi/my-react-project.git:gh-pages

---
+++ precache-manifest.83a6ca755decbf61458b49d500a80ae9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215b84cd46e0e6504aebf05998782217",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.bc6c5e6cfd7ca40676f3.bundle.js"
+  },
+  {
+    "url": "runtime~main.bc6c5e6cfd7ca40676f3.bundle.js"
+  },
+  {
+    "revision": "d3ae915711fcda08c1ae",
+    "url": "static/css/main.ebc6cee6.chunk.css"
+  },
+  {
+    "revision": "77271011f9e20f05084c",
+    "url": "static/css/vendors~main.f47422e6.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.bc6c5e6cfd7ca40676f3.bundle.js"
+  },
+  {
+    "url": "vendors~main.bc6c5e6cfd7ca40676f3.bundle.js.LICENSE.txt"
+  }
+]);
